040d5e84ef8e0aa77161817ff9f340a88a8f77b3
iatanasijevic621rn <115571795+iatanasijevic621rn@users.noreply.github.com>
2025-03-05 14:27:17 +0100

    abstraction/AccountService: fix getAllChecking and getAllFx

--- user-service/src/main/java/rs/banka4/user_service/service/abstraction/AccountService.java
+++ user-service/src/main/java/rs/banka4/user_service/service/abstraction/AccountService.java
@@ -3,6 +3,7 @@ package rs.banka4.user_service.service.abstraction;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import rs.banka4.user_service.dto.AccountDto;
 import rs.banka4.user_service.dto.requests.CreateAccountDto;
 
@@ -15,7 +16,7 @@ public interface AccountService {
     ResponseEntity<AccountDto> getAccount(String token, String id);
     ResponseEntity<Void> createAccount(CreateAccountDto createAccountDto);
     ResponseEntity<Page<AccountDto>> getAll(String firstName, String lastName, String id, PageRequest pageRequest);
-    ResponseEntity<Page<AccountDto>> getAllChecking(PageRequest pageRequest);
-    ResponseEntity<Page<AccountDto>> getAllFx(PageRequest pageRequest);
+    ResponseEntity<Page<AccountDto>> getAllChecking(Authentication auth, PageRequest pageRequest);
+    ResponseEntity<Page<AccountDto>> getAllFx(Authentication auth, PageRequest pageRequest);
 
 }
