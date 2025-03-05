3182175778d02abff184693f4f082ba0c47cf04e
iatanasijevic621rn <115571795+iatanasijevic621rn@users.noreply.github.com>
2025-03-05 14:25:52 +0100

    repositories/AccountRepository: add findAllByClientAndCurrency_CodeNot and findAllByClientAndCurrency_Code

--- user-service/src/main/java/rs/banka4/user_service/repositories/AccountRepository.java
+++ user-service/src/main/java/rs/banka4/user_service/repositories/AccountRepository.java
@@ -3,10 +3,14 @@ package rs.banka4.user_service.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import rs.banka4.user_service.models.Account;
+import rs.banka4.user_service.models.Client;
 import rs.banka4.user_service.models.Currency;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -14,5 +18,7 @@ public interface AccountRepository extends JpaRepository<Account, UUID> {
     Page<Account> findAllByCurrency_Code(Currency.Code currency, PageRequest pageRequest);
     Page<Account> findAllByCurrency_CodeNot(Currency.Code currency, PageRequest pageRequest);
 
+    Page<Account> findAllByClientAndCurrency_Code(Client client, Currency.Code currency, PageRequest pageRequest);
+    Page<Account> findAllByClientAndCurrency_CodeNot(Client client, Currency.Code currency, PageRequest pageRequest);
 
 }
