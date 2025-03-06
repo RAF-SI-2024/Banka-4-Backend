9a9668b3a06c3fd1bcf15535282bdea48c7dea39
Luka Korica <lukakorica@gmail.com>
2025-03-06 22:42:33 +0100

    Added databse migration to create a table for authentication events

---
+++ user-service/src/main/resources/db/migration/V8__authentication_events.sql
@@ -0,0 +1,23 @@
+CREATE TABLE authentication_events
+(
+    id               UUID         NOT NULL,
+    type             VARCHAR(255) NOT NULL,
+    did_authenticate BOOLEAN      NOT NULL,
+    created_at       TIMESTAMP WITHOUT TIME ZONE,
+    updated_at       TIMESTAMP WITHOUT TIME ZONE,
+    client_id        VARCHAR(255),
+    employee_id      VARCHAR(255),
+    CONSTRAINT pk_authentication_events PRIMARY KEY (id)
+);
+
+ALTER TABLE authentication_events
+    ADD CONSTRAINT uc_authentication_events_client UNIQUE (client_id);
+
+ALTER TABLE authentication_events
+    ADD CONSTRAINT uc_authentication_events_employee UNIQUE (employee_id);
+
+ALTER TABLE authentication_events
+    ADD CONSTRAINT FK_AUTHENTICATION_EVENTS_ON_CLIENT FOREIGN KEY (client_id) REFERENCES clients (id);
+
+ALTER TABLE authentication_events
+    ADD CONSTRAINT FK_AUTHENTICATION_EVENTS_ON_EMPLOYEE FOREIGN KEY (employee_id) REFERENCES employees (id);
