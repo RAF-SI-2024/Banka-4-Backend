ae9dab9a5682cde52012f98d02360e160cf68f0c
mbaclija1921rn <mbaclija1921rn@raf.rs>
2025-03-11 22:51:10 +0100

    exchange-office: support offline tests

--- exchange-office/tests/conftest.py
+++ exchange-office/tests/conftest.py
@@ -1,14 +1,19 @@
 import flask
 import pytest
 import pathlib
+import os.path as path
 
 from banka4_exchange import create_app
 
+@pytest.fixture(autouse=True)
+def disable_remake(monkeypatch):
+    monkeypatch.setattr("banka4_exchange.should_remake", lambda: False)
 
 @pytest.fixture()
-def app(tmpdir: pathlib.Path) -> flask.Flask:
+def app() -> flask.Flask:
     app = create_app()
     app.config["COMMISSION_RATE"] = 0.1
-    app.config["EXCHANGERATE_API_KEY"] = "280153fe0016f484aedcecdd"
-    app.config["EXCHANGE_STORAGE_PATH"] = str(tmpdir / "exchanges.json")
+    app.config["EXCHANGERATE_API_KEY"] = "WRONGAPIKEY"
+    project_root = path.abspath(path.join(path.dirname(__file__), ".."))
+    app.config["EXCHANGE_STORAGE_PATH"] = path.join(project_root, "tests/test_exchange.json")
     return app
