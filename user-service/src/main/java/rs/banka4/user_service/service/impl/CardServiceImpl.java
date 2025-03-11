8a9698bb8743e806bba6b3b4cb7c7d69368796c5
iatanasijevic621rn <115571795+iatanasijevic621rn@users.noreply.github.com>
2025-03-11 18:30:49 +0100

    service/impl/CardServiceImpl implement block endpoint

--- user-service/src/main/java/rs/banka4/user_service/service/impl/CardServiceImpl.java
+++ user-service/src/main/java/rs/banka4/user_service/service/impl/CardServiceImpl.java
@@ -1,14 +1,23 @@
 package rs.banka4.user_service.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import rs.banka4.user_service.domain.card.db.Card;
+import rs.banka4.user_service.domain.card.db.CardStatus;
 import rs.banka4.user_service.domain.card.dtos.CardDto;
 import rs.banka4.user_service.domain.card.dtos.CreateCardDto;
+import rs.banka4.user_service.repositories.CardRepository;
 import rs.banka4.user_service.service.abstraction.CardService;
 
+import java.util.Optional;
+@Service
+@RequiredArgsConstructor
 public class CardServiceImpl implements CardService {
+    private final CardRepository cardRepository;
+
     @Override
     public Card createAuthorizedCard(CreateCardDto createCardDto) {
         return null;
@@ -16,8 +25,22 @@ public class CardServiceImpl implements CardService {
 
     @Override
     public Card blockCard(String cardNumber) {
-        return null;
+
+        Optional<Card> optionalCard = cardRepository.findCardByCardNumber(cardNumber);
+
+        if (optionalCard.isEmpty()) {
+            return null;
+        }
+        Card card = optionalCard.get();
+
+        if (card.getCardStatus().equals(CardStatus.BLOCKED) || card.getCardStatus() == CardStatus.DEACTIVATED) {
+            return card;
+        }
+
+        card.setCardStatus(CardStatus.BLOCKED);
+        return cardRepository.save(card);
     }
+
 
     @Override
     public Card unblockCard(String cardNumber) {
