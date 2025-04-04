ade4327c3290fd7682d8c5966ae16632e82b6a40
Arsenije <apetrovic10322rn@raf.rs>
2025-04-04 17:48:01 +0200

    stock-service/add-new-api-calls

--- stock-service/src/main/java/rs/banka4/stock_service/config/retrofit/AlphaVantageService.java
+++ stock-service/src/main/java/rs/banka4/stock_service/config/retrofit/AlphaVantageService.java
@@ -4,7 +4,9 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.Query;
+import rs.banka4.stock_service.domain.listing.dtos.ListingApiDto;
 import rs.banka4.stock_service.domain.security.forex.dtos.ForexPairApiDto;
+import rs.banka4.stock_service.domain.security.stock.dtos.StockInfoDto;
 
 
 public interface AlphaVantageService {
@@ -14,6 +16,22 @@ public interface AlphaVantageService {
         @Query("function") String function,
         @Query("from_currency") String symbol1,
         @Query("to_currency") String symbol2,
+        @Query("apikey") String apiKey
+    );
+
+    @Headers("Accept: application/json")
+    @GET("query")
+    Call<StockInfoDto> getStockInfo(
+        @Query("function") String function,
+        @Query("symbol") String ticker,
+        @Query("apikey") String apiKey
+    );
+
+    @Headers("Accept: application/json")
+    @GET("query")
+    Call<ListingApiDto> getListingInfo(
+        @Query("function") String function,
+        @Query("symbol") String symbol,
         @Query("apikey") String apiKey
     );
 }
