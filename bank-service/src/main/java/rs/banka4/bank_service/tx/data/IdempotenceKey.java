b3a1aa3d1a6cced8efe4e832c04cd72e0bb1b2d6
Arsen Arsenović <aarsenovic8422rn@raf.rs>
2025-04-22 15:05:05 +0200

    bank/tx: make IdemKeys embeddable

--- bank-service/src/main/java/rs/banka4/bank_service/tx/data/IdempotenceKey.java
+++ bank-service/src/main/java/rs/banka4/bank_service/tx/data/IdempotenceKey.java
@@ -1,7 +1,11 @@
 package rs.banka4.bank_service.tx.data;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
 public record IdempotenceKey(
-    long routingNumber,
-    String locallyGeneratedKey
+    @Column(nullable = false) long routingNumber,
+    @Column(nullable = false) String locallyGeneratedKey
 ) {
 }
