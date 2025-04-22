b3f9240e6f24f648663c61772e31d7ea6162ec90
Arsen Arsenović <aarsenovic8422rn@raf.rs>
2025-04-22 15:05:19 +0200

    bank/tx: add With to transactions

--- bank-service/src/main/java/rs/banka4/bank_service/tx/data/Transaction.java
+++ bank-service/src/main/java/rs/banka4/bank_service/tx/data/Transaction.java
@@ -1,8 +1,10 @@
 package rs.banka4.bank_service.tx.data;
 
 import java.util.List;
+import lombok.With;
 import rs.banka4.bank_service.domain.trading.db.ForeignBankId;
 
+@With
 public record Transaction(
     List<Posting> postings,
     String message,
