714e42fa0fe73ba80a60649f40337246798e09fb
Arsen Arsenović <aarsenovic8422rn@raf.rs>
2025-04-24 00:28:20 +0200

    bank/tx: validate interbank config

--- bank-service/src/main/java/rs/banka4/bank_service/tx/config/InterbankConfig.java
+++ bank-service/src/main/java/rs/banka4/bank_service/tx/config/InterbankConfig.java
@@ -1,5 +1,7 @@
 package rs.banka4.bank_service.tx.config;
 
+import jakarta.annotation.Nonnull;
+import jakarta.validation.Valid;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,7 +12,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Data
 @NoArgsConstructor
 @ConfigurationProperties("rafeisen.interbank")
+@Valid
 public class InterbankConfig {
+    @Valid
+    @Nonnull
     private Map<Long, SingleBankConfig> routingTable = new HashMap<>();
 
     /**
@@ -19,8 +24,11 @@ public class InterbankConfig {
     private Duration resendDuration = Duration.ofSeconds(15);
 
     @Data
+    @Valid
     public static class SingleBankConfig {
+        @Nonnull
         private String apiKey;
+        @Nonnull
         private String baseUrl;
     }
 }
