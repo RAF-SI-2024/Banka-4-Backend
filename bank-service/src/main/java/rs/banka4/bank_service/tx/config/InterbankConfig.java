1e459240659646962ce134087f8f7cd1ca21664c
Arsen Arsenović <aarsenovic8422rn@raf.rs>
2025-04-23 04:07:01 +0200

    bank/tx: allow configuring message resend interval in IBC

--- bank-service/src/main/java/rs/banka4/bank_service/tx/config/InterbankConfig.java
+++ bank-service/src/main/java/rs/banka4/bank_service/tx/config/InterbankConfig.java
@@ -1,5 +1,6 @@
 package rs.banka4.bank_service.tx.config;
 
+import java.time.Duration;
 import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +13,11 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties("rafeisen.interbank")
 public class InterbankConfig {
     private Map<Long, SingleBankConfig> routingTable;
+
+    /**
+     * How frequently should messages be re-sent?
+     */
+    private Duration resendDuration = Duration.ofSeconds(15);
 
     @Data
     public static class SingleBankConfig {
