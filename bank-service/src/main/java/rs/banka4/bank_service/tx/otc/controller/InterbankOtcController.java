5946e282a28c141ff04b3f16c425029dad79becf
DimitrijeA <53818425+SavkoDinamitas@users.noreply.github.com>
2025-04-23 13:26:48 +0200

    bank/tx: add fetchPublicStocks

--- bank-service/src/main/java/rs/banka4/bank_service/tx/otc/controller/InterbankOtcController.java
+++ bank-service/src/main/java/rs/banka4/bank_service/tx/otc/controller/InterbankOtcController.java
@@ -4,12 +4,14 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import rs.banka4.bank_service.tx.data.PublicStock;
 import rs.banka4.bank_service.tx.otc.service.InterbankOtcService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/interbank")
 public class InterbankOtcController {
 
     private final InterbankOtcService interbankOtcService;
--- bank-service/src/main/java/rs/banka4/bank_service/tx/otc/service/impl/InterbankOtcServiceImpl.java
+++ bank-service/src/main/java/rs/banka4/bank_service/tx/otc/service/impl/InterbankOtcServiceImpl.java
@@ -1,20 +1,25 @@
 package rs.banka4.bank_service.tx.otc.service.impl;
 
+import java.io.IOException;
 import java.util.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import retrofit2.Response;
 import rs.banka4.bank_service.domain.assets.db.AssetOwnership;
 import rs.banka4.bank_service.domain.trading.db.ForeignBankId;
+import rs.banka4.bank_service.exceptions.RequestFailed;
 import rs.banka4.bank_service.repositories.AssetOwnershipRepository;
 import rs.banka4.bank_service.tx.data.PublicStock;
 import rs.banka4.bank_service.tx.data.Seller;
 import rs.banka4.bank_service.tx.data.StockDescription;
+import rs.banka4.bank_service.tx.otc.config.InterbankService;
 import rs.banka4.bank_service.tx.otc.service.InterbankOtcService;
 
 @Service
 @RequiredArgsConstructor
 public class InterbankOtcServiceImpl implements InterbankOtcService {
     private final AssetOwnershipRepository assetOwnershipRepository;
+    private final InterbankService interbankRetrofit;
 
     /**
      * Fetches all asset ownership from our bank, filters only ones that have publicAmount > 0 and
@@ -28,7 +33,6 @@ public class InterbankOtcServiceImpl implements InterbankOtcService {
                 .filter(ow -> ow.getPublicAmount() > 0)
                 .sorted(Comparator.comparing(AssetOwnership::getTicker))
                 .toList();
-        Set<String> tickers = new HashSet<>();
         List<PublicStock> publicStocks = new ArrayList<>();
         for (int i = 0; i < publicOwnerships.size(); i++) {
             if (
@@ -86,6 +90,12 @@ public class InterbankOtcServiceImpl implements InterbankOtcService {
 
     @Override
     public List<PublicStock> fetchPublicStocks() {
-        return List.of();
+        try {
+            var call = interbankRetrofit.getPublicStocks();
+            Response<List<PublicStock>> response = call.execute();
+            return response.body();
+        } catch (IOException e) {
+            throw new RequestFailed();
+        }
     }
 }
