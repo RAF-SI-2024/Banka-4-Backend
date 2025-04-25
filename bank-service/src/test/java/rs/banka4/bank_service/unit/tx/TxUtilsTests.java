f432c80c6e0b15866757516b8aeefe32f85aba94
Arsen Arsenović <aarsenovic8422rn@raf.rs>
2025-04-25 05:30:58 +0200

    bank/tx: test TxUtils#isTxBalanced

---
+++ bank-service/src/test/java/rs/banka4/bank_service/unit/tx/TxUtilsTests.java
@@ -0,0 +1,69 @@
+package rs.banka4.bank_service.unit.tx;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import rs.banka4.bank_service.tx.TxUtils;
+import rs.banka4.bank_service.tx.data.DoubleEntryTransaction;
+import rs.banka4.bank_service.tx.data.Posting;
+import rs.banka4.bank_service.tx.data.TxAccount;
+import rs.banka4.bank_service.tx.data.TxAsset;
+import rs.banka4.rafeisen.common.currency.CurrencyCode;
+
+public class TxUtilsTests {
+    @Test
+    void test_isTxBalanced_empty() {
+        assertThat(TxUtils.isTxBalanced(new DoubleEntryTransaction(List.of(), null, null)))
+            .isTrue();
+    }
+
+    @Test
+    void test_isTxBalanced_basic() {
+        assertThat(
+            TxUtils.isTxBalanced(
+                new DoubleEntryTransaction(
+                    List.of(
+                        new Posting(
+                            new TxAccount.MemoryHole(),
+                            new BigDecimal(5),
+                            new TxAsset.Monas(CurrencyCode.CAD)
+                        ),
+                        new Posting(
+                            new TxAccount.MemoryHole(),
+                            new BigDecimal(5).negate(),
+                            new TxAsset.Monas(CurrencyCode.CAD)
+                        )
+                    ),
+                    null,
+                    null
+                )
+            )
+        ).isTrue();
+    }
+
+    @Test
+    void test_isTxBalanced_disbalance() {
+        assertThat(
+            TxUtils.isTxBalanced(
+                new DoubleEntryTransaction(
+                    List.of(
+                        new Posting(
+                            new TxAccount.MemoryHole(),
+                            new BigDecimal(5),
+                            new TxAsset.Monas(CurrencyCode.CAD)
+                        ),
+                        new Posting(
+                            new TxAccount.MemoryHole(),
+                            new BigDecimal(6).negate(),
+                            new TxAsset.Monas(CurrencyCode.CAD)
+                        )
+                    ),
+                    null,
+                    null
+                )
+            )
+        ).isFalse();
+    }
+}
