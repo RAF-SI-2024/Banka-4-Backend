b98fdbb6ae12d0e1039d39f63d7bd2f7281265eb
Arsen Arsenović <aarsenovic8422rn@raf.rs>
2025-04-26 19:30:48 +0200

    bank/tx: add json converter to bank retrofits

--- bank-service/src/main/java/rs/banka4/bank_service/tx/otc/config/InterbankRetrofitProvider.java
+++ bank-service/src/main/java/rs/banka4/bank_service/tx/otc/config/InterbankRetrofitProvider.java
@@ -1,5 +1,6 @@
 package rs.banka4.bank_service.tx.otc.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,6 +8,7 @@ import java.util.Optional;
 import okhttp3.OkHttpClient;
 import org.springframework.stereotype.Service;
 import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 import rs.banka4.bank_service.tx.config.InterbankConfig;
 
 @Service
@@ -15,7 +17,7 @@ public class InterbankRetrofitProvider {
     private final Map<Long, Retrofit> retrofits = new HashMap<>();
     private final Map<Long, InterbankService> interbanks = new HashMap<>();
 
-    public InterbankRetrofitProvider(InterbankConfig cfg) {
+    public InterbankRetrofitProvider(InterbankConfig cfg, ObjectMapper objectMapper) {
         for (
             final var route : cfg.getRoutingTable()
                 .entrySet()
@@ -36,6 +38,7 @@ public class InterbankRetrofitProvider {
             clients.put(bankId, client);
             final var retrofit =
                 new Retrofit.Builder().client(client)
+                    .addConverterFactory(JacksonConverterFactory.create(objectMapper))
                     .baseUrl(bankCfg.getBaseUrl())
                     .build();
             retrofits.put(bankId, retrofit);
